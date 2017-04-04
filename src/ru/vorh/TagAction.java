780992174821f9581ccde24585f410758fd3f747
yaroslav.pavlov <yaroslav.pavlov@aisa.ru>
2017-04-04 15:09:49 +0300

    ADD [ add simple modal and point in menu ]

--- src/ru/vorh/TagAction.java
+++ src/ru/vorh/TagAction.java
@@ -2,9 +2,17 @@ package ru.vorh;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ShortcutSet;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revwalk.RevCommit;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
 /**
  * Created by yaroslav on 3/31/17.
@@ -14,11 +22,14 @@ public class TagAction extends AnAction {
     private ModalTag modalTag;
     private GitService gitService;
     private Git git;
+    private Project project;
 
-    public TagAction() {
-
+    public TagAction(Project project) {
+        Presentation templatePresentation = getTemplatePresentation();
+        templatePresentation.setText("Last tags");
+        templatePresentation.setDescription("Extract last tags");
         try {
-            git = Git.open(new java.io.File(""));
+            git = Git.open(new File(project.getBasePath()));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,5 +38,20 @@ public class TagAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
+
+        RevCommit lastCommit = gitService.getLastCommit(git.getRepository());
+        Set<Ref> tagCommit = gitService.getTagCommit(git.getRepository(), lastCommit);
+        StringBuilder tags = new StringBuilder();
+        tagCommit.forEach(ref -> {
+            tags.append(ref.getName().replace("refs/tags/",""));
+            tags.append(" ");
+        });
+
+        Messages.showWarningDialog(tags.toString(), "Last Tags");
+    }
+
+    @Override
+    protected void setShortcutSet(ShortcutSet shortcutSet) {
+        super.setShortcutSet(shortcutSet);
     }
 }
--- src/ru/vorh/TagProjectComponent.java
+++ src/ru/vorh/TagProjectComponent.java
@@ -1,21 +1,49 @@
 package ru.vorh;
 
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 /**
  * Created by yaroslav on 3/31/17.
  */
 public class TagProjectComponent implements ProjectComponent {
+    private Project project;
 
     public TagProjectComponent(Project project) {
+        this.project = project;
 
     }
 
     @Override
     public void initComponent() {
+
+        System.out.println("CREATE COMPONENT ");
+        ActionManager actionManager = ActionManager.getInstance();
+        TagAction tagAction = new TagAction(project);
+
+        KeyboardShortcut keyboardShortcut = new KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.CTRL_MASK,1),KeyStroke.getKeyStroke("]"));
+        CustomShortcutSet customShortcutSet = new CustomShortcutSet(keyboardShortcut);
+        tagAction.setShortcutSet(customShortcutSet);
+
+        DefaultActionGroup actionGroup = (DefaultActionGroup) actionManager.getAction("VcsGroups");
+        actionGroup.add(tagAction);
+        AnAction[] childActionsOrStubs = actionGroup.getChildActionsOrStubs();
+        System.out.println(tagAction.getTemplatePresentation().getText());
+
+        for (AnAction anAction : Arrays.asList(childActionsOrStubs)) {
+            System.out.println(anAction.toString());
+            System.out.println("");
+        }
+        actionManager.registerAction("TagAction",tagAction);
+
         // TODO: insert component initialization logic here
+        System.out.println("FINISH SETTING COMPONENT");
     }
 
     @Override
